a3d7b6b6df48dbfddcbab8cb32e344ff92c285c6
Mohammad Ahmady Yeganeh <m.yegane49@outlook.com>
2020-02-09 19:22:25 +0330

    Continue on until 5-4

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 import './main.scss';
 import 'bootstrap';
+import jQuery from 'jquery';
+
+jQuery('.carousel').carousel({
+    interval: 6000,
+    keyboard: true,
+    pause: 'hover',
+    wrap: true
+});
+
+jQuery('#slider4').on('slide.bs.carousel', () => {
+    console.log('SLIDE!');
+});
+
+jQuery('#slider4').on('slid.bs.carousel', () => {
+    console.log('SLID!');
+});
